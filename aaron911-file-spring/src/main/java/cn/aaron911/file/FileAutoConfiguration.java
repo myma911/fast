c1f0c42086d18d4731dbf21228dc1a6683dd4287
Aaron <450870442@qq.com>
2020-09-16 11:13:45 +0800

    2020.9.16

--- aaron911-file-spring/src/main/java/cn/aaron911/file/FileAutoConfiguration.java
+++ aaron911-file-spring/src/main/java/cn/aaron911/file/FileAutoConfiguration.java
@@ -1,11 +1,13 @@
 package cn.aaron911.file;
 
+import javax.annotation.PostConstruct;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import cn.aaron911.file.property.FileProperties;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @version 1.0.0
@@ -13,15 +15,23 @@ import cn.aaron911.file.property.FileProperties;
  */
 @Configuration
 @EnableConfigurationProperties(FileProperties.class)
+@Slf4j
 public class FileAutoConfiguration {
+	
+	@PostConstruct
+	public void init() {
+		log.debug("cn.aaron911.file 文件上传开始初始化");
+	}
 	
     @Bean
     BaseFileUploader baseFileUploader() {
+    	log.debug("cn.aaron911.file 文件上传BaseFileUploader初始化");
         return new BaseFileUploader();
     }
     
     @Bean
     GlobalFileUploader globalFileUploader() {
+    	log.debug("cn.aaron911.file 文件上传GlobalFileUploader初始化");
         return new GlobalFileUploader();
     }
 
